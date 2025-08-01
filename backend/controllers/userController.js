9848befffe14994e9c0f690e4a8d7bf537f14be7
Shatakshi Rajput <shatakshirajput02@gmail.com>
2025-08-01 23:25:14 +0530

    added frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,11 +46,11 @@ async function signup(req, res) {
     const result = await usersCollection.insertOne(newUser);
 
     const token = jwt.sign(
-      { id: result.insertId },
+      { id: result.insertedId },
       process.env.JWT_SECRET_KEY,
       { expiresIn: "1h" }
     );
-    res.json({ token, userId: result.insertId });
+    res.json({ token, userId: result.insertedId });
   } catch (err) {
     console.error("Error during signup : ", err.message);
     res.status(500).send("Server error");
